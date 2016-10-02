8b46aea7ab1f3f0c6e2d29bcb4c433d33a648a95
DjLeChuck <djlechuck@gmail.com>
2016-10-02 22:26:04 +0200

    Move readFile to utils

--- controllers/logs.js
+++ controllers/logs.js
@@ -1,18 +1,7 @@
 module.exports.index = function * () {
-  var fs = require('fs');
-
-  var readFileThunk = function(src) {
-    return new Promise(function (resolve, reject) {
-      fs.readFile(src, {'encoding': 'utf8'}, function (err, data) {
-        if(err) return reject(err);
-        resolve(data);
-      });
-    });
-  }
-
   this.state.logs = {
     path: this.state.config.recalbox.logsPath,
-    content: yield readFileThunk(this.state.config.recalbox.logsPath)
+    content: yield require('../lib/utils').readFile(this.state.config.recalbox.logsPath)
   };
 
   this.state.activePage = 'logs';

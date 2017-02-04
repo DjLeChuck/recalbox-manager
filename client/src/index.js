fbf9db6c7719951e49dc875d98ca8888b282dc17
DjLeChuck <djlechuck@gmail.com>
2017-02-04 12:17:02 +0100

    Add missing semicolon

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoutes from './components/AppRoutes';
-import './dependencies/css/bootstrap.min.css'
-import './dependencies/css/theme.css'
+import './dependencies/css/bootstrap.min.css';
+import './dependencies/css/theme.css';
 import './index.css';
 
 ReactDOM.render(

bae3c2f904eefe7163670e9aa3942f86660b8f76
DjLeChuck <djlechuck@gmail.com>
2017-02-12 09:43:13 +0100

    Add methods to handle ROMs listing

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,6 +2,7 @@ import config from 'config';
 import fs from 'fs';
 import path from 'path';
 import md5File from 'md5-file';
+import xml2js from 'xml2js';
 
 // Traitement d'une ligne du fichier readme.txt des BIOS
 const md5Rule = /^[a-f0-9]{32}$/i;
@@ -24,4 +25,102 @@ export function handleBiosLine(line) {
     name: name,
     valid: fs.existsSync(thisBiosPath) ? md5 === md5File.sync(thisBiosPath) : null
   };
+}
+
+// Traitement des ROMs
+export async function getRoms(system, subpath = '') {
+  const srcpath = path.join(config.get('recalbox.romsPath'), system, subpath);
+  const esSystems = await getEsSystems();
+  const systemData = esSystems.find((s) => s.name === system);
+  const romExtensions = systemData ? systemData.extensions : [];
+
+  return fs.readdirSync(srcpath).filter((file) => {
+    return fs.statSync(path.join(srcpath, file)).isFile() &&
+      -1 !== romExtensions.indexOf(path.extname(file));
+  });
+}
+
+// Promise xml2json
+async function xmlToJson(file) {
+  var promise = await new Promise((resolve, reject) => {
+    const parser = new xml2js.Parser({
+      trim: true,
+      explicitArray: false
+    });
+
+    parser.parseString(fs.readFileSync(file), (jsError, jsResult) => {
+      if (jsError) {
+        reject(jsError);
+      } else {
+        resolve(jsResult);
+      }
+    });
+  });
+
+  return promise;
+}
+
+// Traitements des systèmes supportés par ES
+let esSystems;
+
+export async function getEsSystems() {
+  if (esSystems) {
+    return esSystems;
+  }
+
+  const json = await xmlToJson(config.get('recalbox.esSystemsCfgPath'));
+  esSystems = [];
+
+  json.systemList.system.forEach((system) => {
+    esSystems.push({
+      name: system.name,
+      fullname: system.fullname,
+      path: system.path,
+      extensions: system.extension ? system.extension.split(' ') : [],
+      launchCommand: system.command,
+    });
+  });
+
+  return esSystems;
+}
+
+// Traitement des fichiers gamelist.xml
+let gamelistCollection = {};
+
+export function parseGameReleaseDate(releaseDate) {
+  return {
+    day: releaseDate.substring(6, 8),
+    month: releaseDate.substring(4, 6),
+    year: releaseDate.substring(0, 4),
+  };
+}
+
+export function getSystemRomsBasePath(system) {
+  return path.join(config.get('recalbox.romsPath'), system);
+}
+
+export function getSystemGamelistPath(system) {
+  return path.join(getSystemRomsBasePath(system), 'gamelist.xml');
+}
+
+export async function getSystemGamelist(system) {
+  if (gamelistCollection.system) {
+    return gamelistCollection.system;
+  }
+
+  const json = await xmlToJson(getSystemGamelistPath(system));
+  let list = {};
+  let gameList = json.gameList.game || [];
+
+  if (!Array.isArray(gameList)) {
+    gameList = [gameList];
+  }
+
+  gameList.forEach((game) => {
+    list[game.path.substring(2)] = game;
+  });
+
+  gamelistCollection.system = list;
+
+  return list;
 }
--- src/routes/get.js
+++ src/routes/get.js
@@ -2,13 +2,23 @@ import express from 'express';
 import config from 'config';
 import fs from 'fs';
 import path from 'path';
-import { handleBiosLine } from '../lib/utils';
+import {
+  handleBiosLine, getEsSystems, getRoms, getSystemGamelist,
+  parseGameReleaseDate
+} from '../lib/utils';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const option = req.query.option;
-  const param = req.query.param || {};
+  const params = {};
+  const queryParams = req.query.params || '';
+
+  queryParams.split(',').map((param) => {
+    const parts = param.split('=');
+
+    params[[parts[0]]] = parts[1];
+  });
   let data;
   let srcpath;
 
@@ -19,21 +29,21 @@ router.get('/', (req, res) => {
         port: req.app.get('port'),
       }
       break;
-    case 'romsDirectories':
-      srcpath = config.get('recalbox.romsPath');
-      data = fs.readdirSync(srcpath).filter((file) => {
-        return fs.statSync(path.join(srcpath, file)).isDirectory();
+    case 'directoryListing':
+      let directoryPath = path.join(config.get('recalbox.romsPath'), params.subpath || '');
+      data = fs.readdirSync(directoryPath).filter((file) => {
+        return fs.statSync(path.join(directoryPath, file)).isDirectory();
       });
 
       // add favorites "virtual" folder
-      if ('addFavorites' === param) {
+      if (params.addFavorites) {
         data.push('favorites');
       }
 
       data.sort();
       break;
     case 'readFile':
-      data = fs.readFileSync(param).toString();
+      data = fs.readFileSync(params.file).toString();
       break;
     case 'biosList':
       const contents = fs.readFileSync(config.get('recalbox.biosFilePath'), 'utf8');
@@ -52,6 +62,57 @@ router.get('/', (req, res) => {
       data = fs.readdirSync(srcpath).filter((file) => {
         return fs.statSync(path.join(srcpath, file)).isFile() && '.png' === path.extname(file);
       });
+      break;
+    case 'systemFullname':
+      const paramSystem = params.system;
+      const esSystems = await getEsSystems();
+      const systemData = esSystems.find((s) => s.name === paramSystem);
+      data = systemData ? systemData.fullname : paramSystem;
+      break;
+    case 'romsList':
+      const system = params.system;
+      const subpath = params.subpath || '';
+      const roms = await getRoms(system, subpath);
+      const gamelist = await getSystemGamelist(system);
+      let list = [];
+      const seekableFields = [
+        'releasedate', 'name', 'image', 'desc', 'genre', 'developer',
+        'publisher', 'players', 'rating',
+      ];
+
+      roms.forEach((romName) => {
+        const filepath = path.join(subpath, romName);
+        let fullname = romName;
+        let romData = {
+          filename: romName,
+          name: romName,
+        };
+
+        if (gamelist[filepath]) {
+          const gamelistData = gamelist[filepath];
+
+          seekableFields.forEach((field) => {
+            if (gamelistData[field]) {
+              let value = gamelistData[field];
+
+              if ('releasedate' === field) {
+                value = parseGameReleaseDate(value);
+              } else if ('image' === field) {
+                value = path.join('/', system, value);
+              }
+
+              romData[field] = value;
+            }
+          });
+        }
+
+        list.push(romData);
+      });
+
+      data = list;
+      break;
+    case 'esSystems':
+      data = await getEsSystems();
       break;
     default:
       throw new Error(`Option "${option}" unknown`);

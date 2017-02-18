d30c24ba31635adccc3914271c64b6e7b661a428
DjLeChuck <djlechuck@gmail.com>
2017-02-18 19:14:44 +0100

    Add a compilation script

--- README.md
+++ README.md
@@ -6,15 +6,10 @@ The API activation step is only needed if you want launch game through the web i
 
 ## Installation from sources
 1. Clone or download the repository: `git clone https://github.com/DjLeChuck/recalbox-manager.git`
-2. Build the project from a computer (not recalbox): `npm buildboth`
-3. Copy the following folders on recalbox: `\recalbox\share\manager` for example
- * /client/build
- * /config/default.js
- * /config/production.js
- * /dist
-4. Activate the API: https://github.com/recalbox/recalbox-api/blob/1.1.x/documentation/activate-on-recalbox.md
-5. Launch the server (connect through SSH on the recalbox): `cd /recalbox/share/manager && NODE_ENV=production PORT=3000 node dist/server.js`
-6. Go on http://recalbox:3000/
+2. Compile the project from a computer (not recalbox): `./compile.sh`
+3. Activate the API: https://github.com/recalbox/recalbox-api/blob/1.1.x/documentation/activate-on-recalbox.md
+4. Launch the server (connect through SSH on the recalbox): `cd /recalbox/share/manager && NODE_ENV=production PORT=3000 node dist/server.js`
+5. Go on http://recalbox:3000/
 
 ## Installation from releases
 1. Download the release: https://github.com/DjLeChuck/recalbox-manager/releases/
---
+++ compile.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+PINK='\033[1;35m'
+NC='\033[0m'
+
+read -r -d '' Banner <<EOF
+${BLUE}########  ######## ${GREEN} ######     ###    ${RED}##       ########  ${PINK} #######  ##     ##
+${BLUE}##     ## ##       ${GREEN}##    ##   ## ##   ${RED}##       ##     ## ${PINK}##     ##  ##   ##
+${BLUE}##     ## ##       ${GREEN}##        ##   ##  ${RED}##       ##     ## ${PINK}##     ##   ## ##
+${BLUE}########  ######   ${GREEN}##       ##     ## ${RED}##       ########  ${PINK}##     ##    ###
+${BLUE}##   ##   ##       ${GREEN}##       ######### ${RED}##       ##     ## ${PINK}##     ##   ## ##
+${BLUE}##    ##  ##       ${GREEN}##    ## ##     ## ${RED}##       ##     ## ${PINK}##     ##  ##   ##
+${BLUE}##     ## ######## ${GREEN} ######  ##     ## ${RED}######## ########  ${PINK} #######  ##     ##${NC}
+EOF
+
+echo -e "$Banner"
+echo
+
+echo -e "${YELLOW}Compiling ${BLUE}re${GREEN}ca${RED}lb${PINK}ox${YELLOW} - web manager${NC}"
+echo
+
+echo -e "${BLUE}Building sources ${RED}(be patient!)${BLUE}...${NC}"
+npm run -s buildboth
+
+echo -e "${BLUE}Copying files into release directory...${NC}"
+rm -rf release
+mkdir -p release/config
+
+cp -R client/build release
+find . -type f -name '*.map' -exec rm {} + # removing useless "map files"
+cp config/default.js release/config
+cp config/production.js release/config
+cp -R dist release
+cp package.json release
+
+echo -e "${BLUE}Installing dependencies...${NC}"
+cd release
+
+npm install --production
+
+echo
+echo -e "${YELLOW}Compilation done! All you need is inside the release folder!${NC}"

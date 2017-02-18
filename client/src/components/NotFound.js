6665fa521efbdb8f1897a25087185ebdc84de9b8
DjLeChuck <djlechuck@gmail.com>
2017-02-18 18:19:18 +0100

    Customize 404 page

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { translate } from 'react-i18next';
 import { Link } from 'react-router';
+import logo from '../logo.png';
 
 class NotFound extends React.Component {
+  static propTypes = {
+    t: PropTypes.func.isRequired,
+  }
+
   render() {
+    const { t } = this.props;
+
     return (
-      <div className="not-found">
+      <div className="not-found text-center">
         <h1>404</h1>
-        <h2>Page not found!</h2>
+        <h2>{t('Page introuvable !')}</h2>
+
+        <img src={logo} alt="Recalbox" />
+
         <p>
-          <Link to="/">Go back to the main page</Link>
+          <Link to="/">{t("Retour à la page d'accueil")}</Link>
         </p>
       </div>
     );
   }
 }
 
-export default NotFound;
+export default translate()(NotFound);

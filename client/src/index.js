52c19422aa239148e133f8666f769a8b694de787
DjLeChuck <djlechuck@gmail.com>
2017-04-09 08:58:38 +0200

    [Client] Add shim for Object.entries

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { I18nextProvider } from 'react-i18next';
+import entries from 'object.entries';
 import AppRoutes from './components/AppRoutes';
 import i18n from './i18n';
 import './dependencies/css/bootstrap.min.css';
 import './dependencies/css/theme.css';
 import './index.css';
+
+if (!Object.entries) {
+  entries.shim();
+}
 
 ReactDOM.render(
   <I18nextProvider i18n={i18n}>

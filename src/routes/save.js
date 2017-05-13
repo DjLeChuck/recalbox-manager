99123dcd86b768dd38cc43f36057bda843dd7645
DjLeChuck <djlechuck@gmail.com>
2017-05-13 14:21:08 +0200

    [Server] Cast boolean to int string

--- src/routes/save.js
+++ src/routes/save.js
@@ -6,13 +6,25 @@ const router = express.Router();
 
 router.post('/', (req, res, next) => {
   try {
-    for (const key in req.body) {
+    let values = {};
+
+    for (const key of Object.keys(req.body)) {
+      let value = req.body[key];
+
+      if (typeof(value) === "boolean") {
+        value = value ? 1 : 0;
+      }
+
+      values[key] = value;
+    }
+
+    for (const key in values) {
       execSync(`${config.get('recalbox.systemSettingsCommand')} -command save -key ${key} -value ${req.body[key]}`);
     }
 
     if (undefined !== req.body['audio.volume'] && 'production' === req.app.get('env')) {
       // Set volume
-      exec(`${config.get('recalbox.configScript')} volume ${req.body['audio.volume']}`, (error, stdout, stderr) => {
+      exec(`${config.get('recalbox.configScript')} volume ${req.body['audio.volume']}`, (error) => {
         if (error) {
           console.error(`exec error: ${error}`);
         }

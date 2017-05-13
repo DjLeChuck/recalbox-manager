a7254abc3df38ab8b11e3f8193b6a4d6e258c879
DjLeChuck <djlechuck@gmail.com>
2017-05-13 13:53:55 +0200

    [Client] Add component to handle form actions

---
+++ client/src/components/forms/FormActions.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { translate } from 'react-i18next';
+import Button from 'react-bootstrap/lib/Button';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+
+const ActionsButtons = ({ t, resetForm, saving }) => (
+  <p>
+    <Button bsStyle="danger" onClick={resetForm}>
+      {t('Annuler')}
+    </Button>{" "}
+    <Button bsStyle="success" type="submit" disabled={saving}>
+      {saving &&
+        <Glyphicon glyph="refresh" className="glyphicon-spin" />
+      } {t('Enregistrer')}
+    </Button>
+  </p>
+);
+
+ActionsButtons.propTypes = {
+  t: PropTypes.func.isRequired,
+  resetForm: PropTypes.func.isRequired,
+  saving: PropTypes.bool.isRequired,
+};
+
+ActionsButtons.defaultProps = {
+  saving: false,
+};
+
+export default translate()(ActionsButtons);

495d6605434315abc920a274cd50785eb6a38aaf
DjLeChuck <djlechuck@gmail.com>
2017-05-14 15:27:27 +0200

    [Client] Use stateless component for AppRoutes

--- client/src/components/AppRoutes.js
+++ client/src/components/AppRoutes.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { Router, browserHistory } from 'react-router';
 import routes from '../routes';
 
-class AppRoutes extends React.Component {
-  render() {
-    return (
-      <Router history={browserHistory} routes={routes} />
-    );
-  }
-}
+const AppRoutes = () => (
+  <Router history={browserHistory} routes={routes} />
+);
 
 export default AppRoutes;
